6042cc7d72e0ac59464969a4820af5ec2aee6d7e
CodyKobe <pawel.tutak@outlook.com>
2017-12-17 19:19:12 +0100

    lets some interaction today - including .js

---
+++ js/ikea.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+	bold();
+
+	function bold(){
+		$('.answer').each(function(){
+			var paragraph = $(this).text();
+			paragraph = paragraph.replace("Iza: ", "<br><b>Iza:&nbsp;</b>");
+			paragraph = paragraph.replace("Radek: ", "<br><b>Radek:&nbsp;</b>");
+			paragraph = paragraph.replace("Iza: ", "<br><b>Iza: </b>");
+			paragraph = paragraph.replace("Radek: ", "<br><b>Radek: </b>");
+			$(this).html(paragraph);
+		});
+	}
+
+
+	function throwImg(){
+		$('img').each(function(){
+			$(this).add(parent).addClass("img-contener");
+			$(this).parent().after().html("<img src=../img/ico/plus.png> class='plus'>");
+		});	  	
+	}
+
+
+
+
+
+
+});
+
